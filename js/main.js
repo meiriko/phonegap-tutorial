2e4b8336317f6ed76f80135f3aa2ea32f346b636
Meir Shahar <meir.shahar@gmail.com>
2012-12-24 09:15:25 +0000

    .

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ var app = {
 	    var self = this;
 	    // Check of browser supports touch events...
 	    if (document.documentElement.hasOwnProperty('ontouchstart')) {
-alert('has touch');
 		// ... if yes: register touch event listener to change the "selected" state of the item
 		$('body').on('touchstart', 'a', function(event) {
 		    $(event.target).addClass('tappable-active');
@@ -20,7 +19,6 @@ alert('has touch');
 		    $(event.target).removeClass('tappable-active');
 		});
 	    } else {
-alert('no touch');
 		// ... if not: register mouse events instead
 		$('body').on('mousedown', 'a', function(event) {
 		    $(event.target).addClass('tappable-active');
